ad991b78112c4a1ad8d6c5186507ae33bfbf54f3
Aidan Lister <aidan@php.net>
2010-11-13 18:28:29 +0000

    Add new templatetag sexyness

--- django_facebook/templatetags/facebook.py
+++ django_facebook/templatetags/facebook.py
@@ -2,11 +2,31 @@ from django import template
 from django.conf import settings
 register = template.Library()
 
-@register.inclusion_tag('facebook_init.html')
-def facebook_init():
-  try:
-    app_id = settings.FACEBOOK_APP_ID    
-  except AttributeError:
-    raise template.TemplateSyntaxError, "%r tag requires FACEBOOK_APP_ID to be configured." \
-      % token.contents.split()[0]
- return {'app_id': app_id}
+@register.inclusion_tag('tags/facebook_load.html')
+def facebook_load():
+    pass
+
+@register.tag
+def facebook_init(parser, token):
+    nodelist = parser.parse(('end_facebook_init',))
+    parser.delete_first_token()
+    return FacebookNode(nodelist)
+
+class FacebookNode(template.Node):
+    """ Allow code to be added inside the facebook asynchronous closure. """
+    def __init__(self, nodelist):
+        try:
+            app_id = settings.FACEBOOK_APP_ID    
+        except AttributeError:
+            raise template.TemplateSyntaxError, "%r tag requires FACEBOOK_APP_ID to be configured." \
+                % token.contents.split()[0]
+        self.app_id   = app_id
+        self.nodelist = nodelist
+        
+    def render(self, context):
+        t = template.loader.get_template('tags/facebook_init.html')
+        code = self.nodelist.render(context)
+        custom_context = context
+        custom_context['code'] = code
+        custom_context['app_id'] = self.app_id
+        return t.render(context)

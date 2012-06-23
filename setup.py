d0cfea4f3d79561d62674ebd46beb442564ec830
Martey Dodoo <martey@marteydodoo.com>
2012-06-23 09:41:31 -0400

    Update setup.py for pypi upload.
    The name of the package needs to change to something other than
    django-facebook in order not to conflict with Thierry Schellenbach's
    package.

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-facebook',
-    version='0.1',
+    name='django4facebook',
+    version='0.1.0',
     description='Replace Django Authentication with Facebook',
     long_description=open('README.md').read(),
     author='Aidan Lister',
     author_email='aidan@php.net',
+    maintainer='Martey Dodoo',
+    maintainer_email='django4facebook@marteydodoo.com',
     url='http://github.com/pythonforfacebook/django-facebook',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'django>=1.2.7',
-        'facebook-sdk>=0.2.0,==dev',
-    ],
-    dependency_links=[
-        'https://github.com/pythonforfacebook/facebook-sdk/tarball/master#egg=facebook-sdk-dev',
+        'facebook-sdk>0.2.0,>=0.3.1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
